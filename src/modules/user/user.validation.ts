41c12641d5f8af14c6f483fd9564c7d7a7b99643
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-11-29 20:57:17 +0600

    validation schema refactored

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -1,14 +1,12 @@
 import { z } from "zod";
 
 const userValidationSchema = z.object({
-   id: z.string().max(20, { message: "id must be less than 20 characters" }),
    password: z
-      .string()
-      .max(20, { message: "password must be less than 20 characters" }),
-   needsPasswordChange: z.boolean().default(true),
-   role: z.enum(["admin", "faculty", "student"]),
-   status: z.enum(["in-progress", "blocked"]).default("in-progress"),
-   isDeleted: z.boolean().default(false),
+      .string({
+         invalid_type_error: "password must be a string",
+      })
+      .max(20, { message: "password must be less than 20 characters" })
+      .optional(),
 });
 
 export default userValidationSchema;
