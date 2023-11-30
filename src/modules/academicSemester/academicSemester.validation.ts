dc07c280f56cf25e032d4fc645f55d303316deb3
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-11-30 21:50:42 +0600

    refactored academic semester year data field

--- src/modules/academicSemester/academicSemester.validation.ts
+++ src/modules/academicSemester/academicSemester.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const academicSemesterValidationSchema = z.object({
    body: z.object({
       name: z.enum(["Autumn", "Summer", "Fall"]),
-      year: z.date(),
+      year: z.string(),
       code: z.enum(["01", "02", "03"]),
       startMonth: z.enum([
          "january",
