00bbe4a1a19e91e87ea64e3bb880a66e0cd32c68
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-11-30 08:45:34 +0600

    route definition added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import notFound from "./middleware/notFound";
-import { UserRoutes } from "./modules/user/user.route";
+import router from "./routes";
 
 const app: Application = express();
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1/", router);
 
 app.get("/", (req: Request, res: Response) => {
    res.status(200).json({
