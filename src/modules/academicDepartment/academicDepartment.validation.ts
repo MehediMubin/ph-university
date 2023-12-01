17f9541bd3f3819217b8041187cc7a0fe3c92f2f
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-01 15:46:22 +0600

    update validation schema created for academic department

--- src/modules/academicDepartment/academicDepartment.validation.ts
+++ src/modules/academicDepartment/academicDepartment.validation.ts
@@ -13,6 +13,24 @@ const academicDepartmentValidationSchema = z.object({
    }),
 });
 
+const updateAcademicDepartmentValidationSchema = z.object({
+   body: z.object({
+      name: z
+         .string({
+            required_error: "Name is required",
+            invalid_type_error: "Name must be a string",
+         })
+         .optional(),
+      facultyId: z
+         .string({
+            required_error: "FacultyId is required",
+            invalid_type_error: "FacultyId must be a string",
+         })
+         .optional(),
+   }),
+});
+
 export const academicDepartmentValidations = {
    create: academicDepartmentValidationSchema,
+   update: updateAcademicDepartmentValidationSchema,
 };
