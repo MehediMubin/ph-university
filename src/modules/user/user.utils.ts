f3fdc8aee7ff9e8504b76070c5dd4dd5fce85acc
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-01 12:14:38 +0600

    generate id more bug fix

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -6,12 +6,29 @@ const findLastStudentId = async () => {
       createdAt: -1,
    });
 
-   return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+   return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // ID -> year + semeseter code + 4 digit number
 export const generateStudentId = async (payload: TAcademicSemester) => {
-   const initialId = (await findLastStudentId()) || (0).toString();
+   let initialId = (0).toString();
+
+   const lastStudentId = await findLastStudentId();
+
+   const lastStudentSemesterCode = lastStudentId?.slice(4, 6);
+   const lastStudentYear = lastStudentId?.slice(0, 4);
+
+   const currentSemesterCode = payload.code;
+   const currentYear = payload.year.slice(0, 4);
+
+   if (
+      lastStudentId &&
+      lastStudentSemesterCode === currentSemesterCode &&
+      lastStudentYear === currentYear
+   ) {
+      initialId = lastStudentId.slice(6, 10);
+   }
+
    const newId = (Number(initialId) + 1).toString().padStart(4, "0");
 
    const id = `${payload.year.slice(0, 4)}${payload.code}${newId}`;
