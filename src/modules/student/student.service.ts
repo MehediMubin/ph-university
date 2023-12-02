6e6f4d0696f65fa09bd23f86f88742eaefe8ba3c
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-02 12:30:29 +0600

    transcation and rollback implement to delete student

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -1,3 +1,6 @@
+import mongoose from "mongoose";
+import AppError from "../../errors/AppError";
+import { UserModel } from "../user/user.model";
 import { StudentModel } from "./student.model";
 
 const getAllStudents = async () => {
@@ -25,8 +28,38 @@ const getSingleStudent = async (id: string) => {
 };
 
 const deleteSingleStudent = async (id: string) => {
-   const result = await StudentModel.deleteOne({ id: id });
-   return result;
+   const session = await mongoose.startSession();
+   try {
+      session.startTransaction();
+
+      const deleteStudent = await StudentModel.findOneAndUpdate(
+         { id: id },
+         { isDeleted: true },
+         { new: true, session },
+      );
+
+      if (!deleteStudent) {
+         throw new AppError(400, "Student not found");
+      }
+
+      const deleteUser = await UserModel.findOneAndUpdate(
+         { id: id },
+         { isDeleted: true },
+         { new: true, session },
+      );
+
+      if (!deleteUser) {
+         throw new AppError(400, "User not found");
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return deleteStudent;
+   } catch (err) {
+      await session.abortTransaction();
+      session.endSession();
+   }
 };
 
 export const StudentServices = {
