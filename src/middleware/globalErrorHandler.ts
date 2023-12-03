9f5718c886cae190158b79eb5a009d6fe14dda0d
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-03 12:08:26 +0600

    zod error converted

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,17 +1,49 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
+import { ZodError, ZodIssue } from "zod";
+import config from "../config";
+import { TErrorSource } from "../interface/error";
 
-const globalErrorHandler = (
-   error: any,
-   req: Request,
-   res: Response,
-   next: NextFunction,
-) => {
-   const statusCode = error.statusCode || 500;
-   const message = error.message || "Something went wrong";
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+   let statusCode = error.statusCode || 500;
+   let message = error.message || "Something went wrong";
+
+   let errorSources: TErrorSource[] = [
+      {
+         path: "",
+         message: "Something went wrong",
+      },
+   ];
+
+   const handleZodError = (error: ZodError) => {
+
+      const errorSources: TErrorSource[] = error.issues.map(
+         (issue: ZodIssue) => {
+            return {
+               path: issue.path[issue.path.length - 1],
+               message: issue.message,
+            };
+         },
+      );
+
+      return {
+         statusCode,
+         message: "Validation Error!",
+         errorSources,
+      };
+   };
+
+   if (error instanceof ZodError) {
+      const simplifiedError = handleZodError(error);
+      statusCode = simplifiedError.statusCode;
+      message = simplifiedError.message;
+      errorSources = simplifiedError.errorSources;
+   }
+
    res.status(statusCode).json({
       success: false,
       message,
-      error,
+      errorSources,
+      stack: config.node_env === "development" ? error?.stack : null,
    });
    next();
 };
