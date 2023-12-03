9cca597ba90914be9c92b816d6fd2bf46a5f7598
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-03 19:44:29 +0600

    handled unhandled rejection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
 
+let serverVar: Server;
+
 async function server() {
    try {
       await mongoose.connect(config.database_url as string);
-      app.listen(config.port, () => {
+      serverVar = app.listen(config.port, () => {
          console.log(`PH University app listening on port ${config.port}`);
       });
    } catch (error) {
@@ -14,3 +17,9 @@ async function server() {
 }
 
 server();
+
+process.on("unhandledRejection", () => {
+   serverVar.close(() => {
+      process.exit(1);
+   });
+});
