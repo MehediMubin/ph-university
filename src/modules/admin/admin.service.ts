59c351bd3aa863655426f900b517045728323623
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-07 20:31:04 +0600

    admin services implemented

---
+++ src/modules/admin/admin.service.ts
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import { UserModel } from "../user/user.model";
+import { TAdmin } from "./admin.interface";
+import { AdminModel } from "./admin.model";
+
+const getAllAdmins = async () => {
+   const admins = await AdminModel.find();
+   return admins;
+};
+
+const getSingleAdmin = async (id: string) => {
+   const admin = await AdminModel.findOne({ id });
+   return admin;
+};
+
+const updateSingleAdmin = async (id: string, payload: Partial<TAdmin>) => {
+   const { name, ...remainingAdminData } = payload;
+
+   const updatedPayload: Record<string, unknown> = { ...remainingAdminData };
+
+   if (name && Object.keys(name).length) {
+      for (const [key, value] of Object.entries(name)) {
+         updatedPayload[`name.${key}`] = value;
+      }
+   }
+
+   const result = await AdminModel.findOneAndUpdate(
+      { id: id },
+      updatedPayload,
+      {
+         new: true,
+      },
+   );
+   return result;
+};
+
+const deleteSingleAdmin = async (id: string) => {
+   const session = await mongoose.startSession();
+   try {
+      session.startTransaction();
+
+      const deleteAdmin = await AdminModel.findOneAndUpdate(
+         { id: id },
+         { isDeleted: true },
+         { new: true, session },
+      );
+
+      if (!deleteAdmin) {
+         throw new Error("Admin deletion failed");
+      }
+
+      const deleteUser = await UserModel.findOneAndUpdate(
+         { id: id },
+         { isDeleted: true },
+         { new: true, session },
+      );
+
+      if (!deleteUser) {
+         throw new Error("User deletion failed");
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+   } catch (err) {
+      await session.abortTransaction();
+      session.endSession();
+      throw new Error("Admin deletion failed");
+   }
+};
+
+export const AdminServices = {
+   getAllAdmins,
+   getSingleAdmin,
+   updateSingleAdmin,
+   deleteSingleAdmin,
+};
