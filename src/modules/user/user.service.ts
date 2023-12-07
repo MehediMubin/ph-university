5393af342cc4de301ce60d74ff9985af2769b108
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-07 16:46:08 +0600

    create faculty service created

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import config from "../../config";
 import AppError from "../../errors/AppError";
 import { AcademicSemesterModel } from "../academicSemester/academicSemester.model";
+import { FacultyModel } from "../faculty/faculty.model";
 import { StudentModel } from "../student/student.model";
 import { TUser } from "./user.interface";
 import { UserModel } from "./user.model";
@@ -10,7 +11,7 @@ import { generateStudentId } from "./user.utils";
 const createStudent = async (password: string, payload) => {
    const user: Partial<TUser> = {};
 
-   user.password = payload.password || (config.default_password as string);
+   user.password = password || (config.default_password as string);
    user.role = "student";
 
    const admissionSemester = await AcademicSemesterModel.findById(
@@ -52,6 +53,43 @@ const createStudent = async (password: string, payload) => {
    }
 };
 
+const createFaculty = async (password: string, payload) => {
+   const user: Partial<TUser> = {};
+
+   user.password = password || (config.default_password as string);
+   user.role = "faculty";
+
+   const session = await mongoose.startSession();
+   try {
+      session.startTransaction();
+
+      const newUser = await UserModel.create([user], { session });
+
+      if (!newUser.length) {
+         throw new AppError(400, "User creation failed");
+      }
+
+      payload.user = newUser[0]._id;
+      payload.id = newUser[0].id;
+
+      const newFaculty = await FacultyModel.create([payload], { session });
+
+      if (!newFaculty.length) {
+         throw new AppError(400, "Faculty creation failed");
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return newFaculty;
+   } catch (err) {
+      await session.abortTransaction();
+      session.endSession();
+      throw new Error("Faculty creation failed");
+   }
+};
+
 export const UserServices = {
    createStudent,
+   createFaculty,
 };
