9047b60da4a4b3c8127f48d90f10220f239d019d
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-15 10:35:01 +0600

    semester registration model created

---
+++ src/modules/semesterRegistration/semesterRegistration.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import { TSemesterRegistration } from "./semesterRegistration.interface";
+
+const semesterRegistrationSchema = new Schema<TSemesterRegistration>(
+   {
+      academicSemester: {
+         type: Schema.Types.ObjectId,
+         unique: true,
+         ref: "AcademicSemester",
+         required: true,
+      },
+      status: {
+         type: String,
+         enum: ["upcoming", "ongoing", "ended"],
+         default: "upcoming",
+      },
+      startDate: {
+         type: Date,
+         required: true,
+      },
+      endDate: {
+         type: Date,
+         required: true,
+      },
+      minCredit: {
+         type: Number,
+         default: 3,
+      },
+      maxCredit: {
+         type: Number,
+         default: 15,
+      },
+   },
+   { timestamps: true },
+);
+
+export const SemesterRegistrationModel = model<TSemesterRegistration>(
+   "SemesterRegistration",
+   semesterRegistrationSchema,
+);
