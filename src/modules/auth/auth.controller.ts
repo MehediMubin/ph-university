4c940c5aef6b37bb51a5937657816a97a31f3e50
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-18 16:17:29 +0600

    implemented some business logic validation to login service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,17 @@
 import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
 import { AuthService } from "./auth.service";
 
 const loginUser = catchAsync(async (req, res) => {
    const { id, password } = req.body;
    const user = await AuthService.loginUser(id, password);
+
+   sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "User logged in successfully",
+      data: user,
+   });
 });
 
 export const AuthController = {
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,32 @@
+import bcrypt from "bcrypt";
+import AppError from "../../errors/AppError";
+import { UserModel } from "../user/user.model";
+
 const loginUser = async (id: string, password: string) => {
-    console.log(id, password);
+   const isUserExists = await UserModel.findOne({ id });
+   if (!isUserExists) {
+      throw new AppError(404, "User not found");
+   }
+
+   // check if the user is deleted or not
+   if (isUserExists.isDeleted) {
+      throw new AppError(403, "User is deleted");
+   }
+
+   // check if the user is blocked or not
+   if (isUserExists.status === "blocked") {
+      throw new AppError(403, "User is blocked");
+   }
+
+   // check if the password is correct or not
+   const isPasswordMatched = await bcrypt.compare(
+      password,
+      isUserExists.password,
+   );
+   if (!isPasswordMatched) {
+      throw new AppError(403, "Password is incorrect");
+   }
+   console.log(isPasswordMatched);
 };
 
 export const AuthService = {
