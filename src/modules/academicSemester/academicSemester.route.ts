330f512478b6c50389e5c4a2ac8b85045fa3f311
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-02-08 20:28:52 +0600

    auth guard added to some routes

--- src/modules/academicSemester/academicSemester.route.ts
+++ src/modules/academicSemester/academicSemester.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
 import validateRequest from "../../middleware/validateRequest";
+import { USER_ROLE } from "../user/user.constant";
 import { academicSemesterController } from "./academicSemester.controller";
 import { academicSemesterValidations } from "./academicSemester.validation";
 
@@ -8,20 +9,36 @@ const router = Router();
 
 router.post(
    "/create-academic-semester",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(academicSemesterValidations.createAcademicSemester),
    academicSemesterController.createAcademicSemester,
 );
 
 router.get(
    "/",
-   auth("admin"),
+   auth(
+      USER_ROLE.superAdmin,
+      USER_ROLE.admin,
+      USER_ROLE.faculty,
+      USER_ROLE.student,
+   ),
    academicSemesterController.getAllAcademicSemester,
 );
 
-router.get("/:id", academicSemesterController.getSingleAcademicSemester);
+router.get(
+   "/:id",
+   auth(
+      USER_ROLE.superAdmin,
+      USER_ROLE.admin,
+      USER_ROLE.faculty,
+      USER_ROLE.student,
+   ),
+   academicSemesterController.getSingleAcademicSemester,
+);
 
 router.patch(
    "/:id",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(academicSemesterValidations.updateAcademicSemester),
    academicSemesterController.updateSinglAcademicSemester,
 );
--- src/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/modules/semesterRegistration/semesterRegistration.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import auth from "../../middleware/auth";
 import validateRequest from "../../middleware/validateRequest";
+import { USER_ROLE } from "../user/user.constant";
 import { SemesterRegistrationController } from "./semesterRegistration.controller";
 import { SemesterRegistrationValidations } from "./semesterRegistration.validation";
 
@@ -7,25 +9,43 @@ const router = Router();
 
 router.post(
    "/create-semester-registration",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(SemesterRegistrationValidations.create),
    SemesterRegistrationController.createSemesterRegistration,
 );
 
-router.get("/", SemesterRegistrationController.getAllSemesterRegistrations);
+router.get(
+   "/",
+   auth(
+      USER_ROLE.superAdmin,
+      USER_ROLE.admin,
+      USER_ROLE.faculty,
+      USER_ROLE.student,
+   ),
+   SemesterRegistrationController.getAllSemesterRegistrations,
+);
 
 router.get(
    "/:id",
+   auth(
+      USER_ROLE.superAdmin,
+      USER_ROLE.admin,
+      USER_ROLE.faculty,
+      USER_ROLE.student,
+   ),
    SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.patch(
    "/:id",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(SemesterRegistrationValidations.update),
    SemesterRegistrationController.updateSemesterRegistration,
 );
 
 router.delete(
    "/:id",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    SemesterRegistrationController.deleteSemesterRegistration,
 );
 
