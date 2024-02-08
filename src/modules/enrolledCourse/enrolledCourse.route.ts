6f6e7936523fcd02532ead07958b1900cefa235e
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-02-08 21:55:21 +0600

    auth guard added to enrolled course & offered course route

--- src/modules/enrolledCourse/enrolledCourse.route.ts
+++ src/modules/enrolledCourse/enrolledCourse.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
 import validateRequest from "../../middleware/validateRequest";
+import { USER_ROLE } from "../user/user.constant";
 import { EnrolledCourseController } from "./enrolledController";
 import { EnrolledCourseValidations } from "./enrolledCourse.validation";
 
@@ -8,14 +9,14 @@ const router = Router();
 
 router.post(
    "/create-enrolled-course",
-   auth("student"),
+   auth(USER_ROLE.student),
    validateRequest(EnrolledCourseValidations.create),
    EnrolledCourseController.createEnrolledCourse,
 );
 
 router.patch(
    "/update-enrolled-course-marks",
-   auth("faculty"),
+   auth(USER_ROLE.faculty, USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(EnrolledCourseValidations.updateMarks),
    EnrolledCourseController.updateEnrolledCourseMarks,
 );
--- src/modules/offeredCourse/offeredCourse.route.ts
+++ src/modules/offeredCourse/offeredCourse.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import auth from "../../middleware/auth";
 import validateRequest from "../../middleware/validateRequest";
+import { USER_ROLE } from "../user/user.constant";
 import { OfferedCourseController } from "./offeredCourse.controller";
 import { OfferedCourseValidations } from "./offeredCourse.validation";
 
@@ -7,20 +9,44 @@ const router = Router();
 
 router.post(
    "/create-offered-course",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(OfferedCourseValidations.create),
    OfferedCourseController.createOfferedCourse,
 );
 
 router.patch(
    "/:id",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
    validateRequest(OfferedCourseValidations.update),
    OfferedCourseController.updateOfferedCourse,
 );
 
-router.get("/", OfferedCourseController.getAllOfferedCourses);
+router.get(
+   "/",
+   auth(
+      USER_ROLE.admin,
+      USER_ROLE.superAdmin,
+      USER_ROLE.student,
+      USER_ROLE.faculty,
+   ),
+   OfferedCourseController.getAllOfferedCourses,
+);
 
-router.get("/:id", OfferedCourseController.getSingleOfferedCourse);
+router.get(
+   "/:id",
+   auth(
+      USER_ROLE.admin,
+      USER_ROLE.superAdmin,
+      USER_ROLE.student,
+      USER_ROLE.faculty,
+   ),
+   OfferedCourseController.getSingleOfferedCourse,
+);
 
-router.delete("/:id", OfferedCourseController.deleteOfferedCourse);
+router.delete(
+   "/:id",
+   auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+   OfferedCourseController.deleteOfferedCourse,
+);
 
 export const OfferedCourseRoutes = router;
